a3a4c47a4101415815c5807be5476100ac87da5d
Jan Helge Aalbu <31095620+jhaalbu@users.noreply.github.com>
2023-11-05 15:58:48 +0100

    Update hendelseklima.py

--- hendelseklima.py
+++ hendelseklima.py
@@ -1,7 +1,11 @@
 import streamlit as st
 
-# Insert JavaScript in the app to redirect immediately
-st.markdown(
-    '<script>window.location.href = "https://geotoolz.streamlit.app/Skredvær";</script>',
-    unsafe_allow_html=True,
-)
+import streamlit as st
+
+# You can generate a button and perform the redirection if it's clicked
+if st.button('Sida er flytta, trykk på meg for å komme til ny side.'):
+    # Use Streamlit's function to render HTML
+    st.markdown(
+        '<meta http-equiv="refresh" content="0;url=https://geotoolz.streamlit.app/" />',
+        unsafe_allow_html=True,
+    )
